eabf98c9124b9f356875bba9ecd43ac76eed07a6
Jerson Vargas <sovaparagon@gmail.com>
2025-04-14 09:27:04 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hackathon_2025_Filipino_Homes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
