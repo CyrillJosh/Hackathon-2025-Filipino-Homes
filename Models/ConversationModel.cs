c70ee1a2ffd4c4c8d8a584a2b2e0917766eb49f8
Jerson Vargas <sovaparagon@gmail.com>
2025-04-15 10:47:32 +0800

    chat new

---
+++ Models/ConversationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon_2025_Filipino_Homes.Models
+{
+    public class ConversationModel
+    {
+        public string OtherUserId { get; set; }
+        public Message LastMessage { get; set; }
+        public string OtherUserFirstName { get; set; }
+        public string OtherUserLastName { get; set; }
+    }
+}
